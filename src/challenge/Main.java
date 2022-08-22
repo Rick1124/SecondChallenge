3ee990eb726c37b2f43d9b25674271626ab07407
Rick1124 <38147073+Rick1124@users.noreply.github.com>
2022-08-22 13:51:56 -0500

    1.1

--- src/challenge/Main.java
+++ src/challenge/Main.java
@@ -1,2 +1,17 @@
-package challenge;public class Main {
+package challenge;
+
+public class Main {
+    public static void main(String[] args) {
+        int solution = SearchingChallenge(10);
+        System.out.println(solution);
+    }
+    public static int SearchingChallenge(int num){
+        int sum = 0;
+        for(int i=1; i<num; i++){
+            if(i%3==0 || i%5==0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
